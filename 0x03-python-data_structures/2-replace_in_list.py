85a7af9b4fc11c225b0cc9f7bb81a6fdc55ea01a
Stace-cmd <stacykerongo@gmail.com>
2023-08-22 05:33:58 -0700

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
+# 2-replace_in_list.py
 
+
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
