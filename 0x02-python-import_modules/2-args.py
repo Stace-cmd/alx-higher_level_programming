5a6f0df08d937a7ef7fd7dd0215b8564225359ed
Stace-cmd <stacykerongo@gmail.com>
2023-08-22 03:16:26 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-#  Prints the number of and the list of its arguments
+# Prints the number of and the list of its arguments
+
 if __name__ == "__main__":
     import sys
 
@@ -7,7 +8,7 @@ if __name__ == "__main__":
     size = len(arg) - 1
 
     if size > 1:
-print("{} arguments:".format(size))
+        print("{} arguments:".format(size))
         for i in range(1, size + 1):
             print("{}: {}".format(i, arg[i]))
 
