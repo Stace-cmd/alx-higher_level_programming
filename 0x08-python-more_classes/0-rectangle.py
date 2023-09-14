a34c333522ad6d5ecee62ab1e685164e87cbba9d
Stace-cmd <stacykerongo@gmail.com>
2023-09-14 01:17:17 -0700

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+Your account doesn’t support creating new files within Google Drive. You can still view and edit existing files.Learn more
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
